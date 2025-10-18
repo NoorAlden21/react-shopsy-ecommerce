8731d27eed631820d17290b1ad64dbe37a80b6cc
NoorAlden <NoorAldenBn3@gmail.com>
2025-10-18 18:48:47 +0300

    first commit

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,49 @@
+import LightButton from "../assets/website/original-3e9fb970a4e76d8d9e99ab30ac14ab9a.png";
+import DarkButton from "../assets/website/Is-dark-mode-better-for-you (1).png";
+import { useEffect, useState } from "react";
+
+const DarkMode = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  const element = document.documentElement;
+
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+  return (
+    <div className="relative">
+      <img
+        src={LightButton}
+        alt=""
+        onClick={() => {
+          setTheme(theme === "dark" ? "light" : "dark");
+        }}
+        className={`w-12 cursor-pointer 
+        drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] 
+        transition-all duration-300 absolute top-[1px] right-0 z-10
+        ${theme === "dark" ? "opacity-0" : "opacity-100"}`}
+      />
+
+      <img
+        src={DarkButton}
+        alt=""
+        onClick={() => {
+          setTheme(theme === "dark" ? "light" : "dark");
+        }}
+        className="w-12 cursor-pointer 
+        drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] 
+        transition-all duration-300 "
+      />
+    </div>
+  );
+};
+
+export default DarkMode;
