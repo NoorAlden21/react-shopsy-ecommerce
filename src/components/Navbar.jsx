f611723326d6357d30a4c09cb16cd93a7aaa2df8
NoorAlden <NoorAldenBn3@gmail.com>
2025-10-18 21:59:40 +0300

    add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,59 @@
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import Logo from "../assets/ecommerce-logo-design_624194-152.jpg";
 import { IoMdSearch } from "react-icons/io";
 import DarkMode from "./DarkMode";
+
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 5,
+    name: "Women Wear",
+    link: "/#",
+  },
+  {
+    id: 4,
+    name: "Electrionics",
+    link: "/#",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
+
 const Navbar = () => {
   return (
     <div
@@ -11,7 +63,7 @@ const Navbar = () => {
     >
       {/* upper Navbar */}
       <div className="bg-primary/40 py-2">
-        <div className="container flex justify-between items-center">
+        <div className="container flex justify-around items-center">
           <div>
             <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2 ">
               <img src={Logo} alt="Logo" className="w-10" />
@@ -26,7 +78,8 @@ const Navbar = () => {
                 placeholder="search"
                 className="bg-white text-gray-700 w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300
                  rounded-full border border-gray-300 px-2 py-1 
-                 focus:outline-none focus:border-1 focus:border-primary"
+                 focus:outline-none focus:border-1 focus:border-primary
+                 dark:border-gray-500 dark:bg-gray-800 dark:text-white"
               />
               <IoMdSearch
                 className="text-gray-500 group-hover:text-primary
@@ -55,8 +108,53 @@ const Navbar = () => {
         </div>
       </div>
 
-      {/* lowerw Navbar */}
-      <div></div>
+      {/* lower Navbar */}
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((item) => (
+            <li>
+              <a
+                href={item.link}
+                className="inline-block px-4 hover:text-primary 
+                duration-200"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending Products
+              <span>
+                <FaCaretDown
+                  className="transition-all duration-200
+                    group-hover:rotate-180"
+                />
+              </span>
+            </a>
+            <div
+              className="absolute z-[9999] hidden 
+            group-hover:block w-[150px] rounded-md
+            bg-white text-black p-2
+            shadow-md"
+            >
+              <ul>
+                {DropdownLinks.map((item) => (
+                  <li key={item.id}>
+                    <a
+                      href={item.link}
+                      className="inline-block w-full
+                    rounded-md p-2 hover:bg-primary/20"
+                    >
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
